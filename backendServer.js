19db1bbd5d324de790001cf75a807cb3583dc5cb
R0-M3 <mestryrohan16@gmail.com>
2023-02-12 16:24:13 +0530

    Path Changes

--- backendServer.js
+++ backendServer.js
@@ -3,11 +3,11 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidv4 } = require('uuid');
+var path = require('path');
 
 app.set('view engine', 'ejs')
 
-app.use(express.static('public'))
-
+app.use(express.static(path.join(__dirname, 'public')))
 app.get("/", (req, res) => {
     res.redirect(`/${uuidv4()}`)
 })
